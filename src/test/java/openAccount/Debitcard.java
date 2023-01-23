bf2bff28f6aae6b9f41a4425c7839411dc91e126
Vinay V <Vinay V@DESKTOP-DRH83U0>
2023-01-23 15:43:23 +0530

    1 commit new

---
+++ src/test/java/openAccount/Debitcard.java
@@ -0,0 +1,13 @@
+package openAccount;
+
+import org.testng.annotations.Test;
+
+public class Debitcard 
+{
+	@Test(groups= {"regression"})
+	public void debitcard()
+	{
+		System.out.println("debit card pin genrated");
+	}
+
+}
---
+++ src/test/java/openAccount/OpenAccount.java
@@ -0,0 +1,13 @@
+package openAccount;
+
+import org.testng.annotations.Test;
+
+public class OpenAccount 
+{
+	@Test(groups= {"Smoke"})
+	public void OpenAccount1()
+	{
+		System.out.println("Account created");
+	}
+
+}
